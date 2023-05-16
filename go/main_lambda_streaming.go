2f39d150c4b43618db05e80763fa9affd69a6c42
Felix <23635466+its-felix@users.noreply.github.com>
2023-05-16 04:49:06 +0200

    feat: initial code

---
+++ go/main_lambda_streaming.go
@@ -0,0 +1,15 @@
+//go:build lambda && lambda.norpc
+
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/its-felix/aws-lambda-go-http-adapter/adapter"
+	"github.com/its-felix/aws-lambda-go-http-adapter/handler"
+)
+
+func main() {
+	mux := newServer()
+	h := handler.NewFunctionURLStreamingHandler(adapter.NewVanillaAdapter(mux))
+	lambda.Start(h)
+}
