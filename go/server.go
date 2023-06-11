629b3f07e0c571dbe2a8971da2ce04fff0d840cb
Felix <23635466+its-felix@users.noreply.github.com>
2023-06-11 05:13:19 +0200

    feat: add otel collector

--- go/server.go
+++ go/server.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"github.com/its-felix/aws-lambda-go-http-adapter/handler"
+	"github.com/labstack/echo/v4"
+	"github.com/uptrace/opentelemetry-go-extra/otelzap"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
 	"net/http"
+	"strconv"
 	"time"
 )
 
-func newServer() *http.ServeMux {
+func newLogger() *otelzap.Logger {
+	rootLog, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+
+	return otelzap.New(rootLog, otelzap.WithMinLevel(zap.InfoLevel))
+}
+
+func newServer(tp trace.TracerProvider, propagators propagation.TextMapPropagator) *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/v2/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -22,4 +40,58 @@ func newServer() *http.ServeMux {
 	})
 
 	return mux
+}
+
+func newEchoServer(tp trace.TracerProvider, prop propagation.TextMapPropagator) *echo.Echo {
+	rootLog := newLogger()
+	httpClient := &http.Client{
+		Transport: otelhttp.NewTransport(http.DefaultTransport),
+	}
+
+	app := echo.New()
+	app.Any("/api/v2/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong")
+	})
+
+	app.Any("/api/v2/ping-delayed", func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextPlain)
+		c.Response().WriteHeader(http.StatusOK)
+
+		for i := 0; i < 100; i++ {
+			_, _ = c.Response().Write([]byte(strconv.Itoa(i)))
+			_, _ = c.Response().Write([]byte("\n"))
+			c.Response().Flush()
+
+			time.Sleep(time.Second)
+		}
+
+		return nil
+	})
+
+	app.Any("/otel", func(c echo.Context) error {
+		ctx := c.Request().Context()
+		log := rootLog.Ctx(ctx)
+
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://api.github.com/repos/open-telemetry/opentelemetry-go/releases/latest", nil)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		res, err := httpClient.Do(req)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		err = res.Body.Close()
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		log.Info("invocation finished, returning event")
+		return c.JSONPretty(http.StatusOK, handler.GetSourceEvent(ctx), "\t")
+	})
+
+	mw := otelecho.Middleware("api", otelecho.WithTracerProvider(tp), otelecho.WithPropagators(prop))
+	app.Use(mw)
+
+	return app
 }
