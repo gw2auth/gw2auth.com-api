93d346e9b172bc8b2197e467c657d8d4f5d3f13b
Felix <23635466+its-felix@users.noreply.github.com>
2024-01-27 23:15:38 +0100

    misc: update cockroach testserver version

--- internal/test/db.go
+++ internal/test/db.go
@@ -44,7 +44,7 @@ func (s *Scope) getOrCreateConfig() (*pgxpool.Config, error) {
 	defer s.mutex.Unlock()
 
 	if pTs := s.ts.Load(); pTs == nil {
-		ts, err := testserver.NewTestServer(testserver.CustomVersionOpt("23.1.13"))
+		ts, err := testserver.NewTestServer(testserver.CustomVersionOpt("v23.1.14"))
 		if err != nil {
 			return nil, err
 		}
