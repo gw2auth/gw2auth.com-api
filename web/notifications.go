808ddc458e6c77e56984e8a2fdf47463f7706d30
Leon Linhart <themrmilchmann@gmail.com>
2025-10-09 22:27:23 +0200

    feat: check disabled GW2 endpoints

--- web/notifications.go
+++ web/notifications.go
@@ -2,11 +2,30 @@ package web
 
 import (
 	"encoding/json"
-	"github.com/labstack/echo/v4"
+	"errors"
 	"net/http"
 	"slices"
 	"time"
+
+	"github.com/labstack/echo/v4"
+	"golang.org/x/sync/errgroup"
 )
+
+type apiResult[T any] struct {
+	Value T
+	Error error
+}
+
+type gw2StatusRoute struct {
+	Path   string `json:"path"`
+	Lang   string `json:"lang"`
+	Auth   string `json:"auth"`
+	Active bool   `json:"active"`
+}
+
+type gw2StatusResponse struct {
+	Routes []gw2StatusRoute `json:"routes"`
+}
 
 type gw2EffApiStatusElement struct {
 	Name     string `json:"name"`
@@ -61,47 +80,117 @@ func NotificationsEndpoint(httpClient *http.Client) echo.HandlerFunc {
 
 	return func(c echo.Context) error {
 		ctx := c.Request().Context()
-		req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://status.gw2efficiency.com/api", nil)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, err)
-		}
+		g, ctx := errgroup.WithContext(ctx)
 
-		req.Header.Set("User-Agent", "GW2Auth")
+		gw2Chan := make(chan apiResult[[]string], 1)
+		gw2EffChan := make(chan apiResult[[]string], 1)
 
-		res, err := httpClient.Do(req)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadGateway, err)
-		}
-
-		defer res.Body.Close()
-
-		if res.StatusCode != http.StatusOK {
-			return echo.NewHTTPError(http.StatusBadGateway, res.Status)
-		}
-
-		var gw2EffStatus gw2EffApiStatusResponse
-		if err = json.NewDecoder(res.Body).Decode(&gw2EffStatus); err != nil {
-			return echo.NewHTTPError(http.StatusBadGateway, err)
-		}
-
-		var endpointsWithIssues []string
-		for _, element := range gw2EffStatus.Data {
-			if slices.Contains(relevantEndpoints, element.Name) && (element.Status != http.StatusOK || element.CheckError() || element.Duration >= 15_000) {
-				endpointsWithIssues = append(endpointsWithIssues, element.Name)
+		g.Go(func() error {
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://api.guildwars2.com/v2.json", nil)
+			if err != nil {
+				gw2Chan <- apiResult[[]string]{Error: err}
+				return err
 			}
-		}
+
+			req.Header.Set("User-Agent", "GW2Auth")
+
+			res, err := httpClient.Do(req)
+			if err != nil || res.StatusCode != http.StatusOK {
+				gw2Chan <- apiResult[[]string]{Error: err}
+				return nil
+			}
+
+			var gw2Status gw2StatusResponse
+			if err = json.NewDecoder(res.Body).Decode(&gw2Status); err != nil {
+				gw2Chan <- apiResult[[]string]{Error: err}
+				return nil
+			}
+
+			var disabledEndpoints []string
+			for _, element := range gw2Status.Routes {
+				if slices.Contains(relevantEndpoints, element.Path) && !element.Active {
+					disabledEndpoints = append(disabledEndpoints, element.Path)
+				}
+			}
+
+			gw2Chan <- apiResult[[]string]{Value: disabledEndpoints}
+
+			defer res.Body.Close()
+			return nil
+		})
+
+		g.Go(func() error {
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://status.gw2efficiency.com/api", nil)
+			if err != nil {
+				return err
+			}
+
+			req.Header.Set("User-Agent", "GW2Auth")
+
+			res, err := httpClient.Do(req)
+			if err != nil {
+				gw2Chan <- apiResult[[]string]{Error: err}
+				return nil
+			}
+
+			if res.StatusCode != http.StatusOK {
+				gw2Chan <- apiResult[[]string]{Error: errors.New(res.Status)}
+				return nil
+			}
+
+			var gw2EffStatus gw2EffApiStatusResponse
+			if err = json.NewDecoder(res.Body).Decode(&gw2EffStatus); err != nil {
+				return echo.NewHTTPError(http.StatusBadGateway, err)
+			}
+
+			var endpointsWithIssues []string
+			for _, element := range gw2EffStatus.Data {
+				if slices.Contains(relevantEndpoints, element.Name) && (element.Status != http.StatusOK || element.CheckError() || element.Duration >= 15_000) {
+					endpointsWithIssues = append(endpointsWithIssues, element.Name)
+				}
+			}
+
+			defer res.Body.Close()
+			return nil
+		})
 
 		var notifications []notification
-		if len(endpointsWithIssues) > 0 {
-			notifications = []notification{
-				{
-					Type:    notificationTypeWarning,
-					Header:  "The Guild Wars 2 API experiences issues right now",
-					Content: "Some of the endpoints used by GW2Auth appear to be in a degraded state right now. This might impact your experience with GW2Auth and Applications using GW2Auth. ",
-				},
+
+		gw2Result := <-gw2Chan
+
+		if gw2Result.Error == nil {
+			disabledEndpoints := gw2Result.Value
+
+			if len(disabledEndpoints) > 0 {
+				notifications = []notification{
+					{
+						Type:    notificationTypeError,
+						Header:  "The Guild Wars 2 API is unavailable",
+						Content: "Some of the endpoints used by GW2Auth are currently disabled. This might impact your experience with GW2Auth and Applications using GW2Auth.",
+					},
+				}
+			} else {
+				notifications = make([]notification, 0)
 			}
 		} else {
-			notifications = make([]notification, 0)
+			gw2EffResult := <-gw2EffChan
+
+			if gw2EffResult.Error == nil {
+				return echo.NewHTTPError(http.StatusBadGateway, gw2EffResult.Error)
+			}
+
+			endpointsWithIssues := gw2EffResult.Value
+			if len(endpointsWithIssues) > 0 {
+				notifications = []notification{
+					{
+						Type:    notificationTypeWarning,
+						Header:  "The Guild Wars 2 API experiences issues right now",
+						Content: "Some of the endpoints used by GW2Auth appear to be in a degraded state right now. This might impact your experience with GW2Auth and Applications using GW2Auth.",
+					},
+				}
+			} else {
+				notifications = make([]notification, 0)
+			}
 		}
 
 		now := time.Now()
